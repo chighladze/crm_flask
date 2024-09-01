fe3276d9f0dc4f2122ad6c98aa778f953edcddce
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-09-01 12:13:05 +0400

    fix

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,11 +1,19 @@
-from flask import Blueprint, abort, request, render_template, redirect, url_for, flash
+from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import login_user, login_required, current_user, logout_user
 from ..extensions import db, bcrypt
 from ..forms.users import UserCreateForm, LoginForm
 from ..models.users import Users
 from datetime import datetime
+from urllib.parse import urlparse, urljoin
 
 users = Blueprint('users', __name__)
+
+
+def is_safe_url(target):
+    from urllib.parse import urlparse, urljoin
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
 
 
 @users.route('/login', methods=['GET', 'POST'])
@@ -15,11 +23,13 @@ def login():
         user = Users.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.passwordHash, form.password.data):
             login_user(user, remember=True)
-            flash("Вы успешно авторизованы", "success")
+            flash("You have successfully logged in.", "success")
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('dashboard.index'))
+            if next_page and is_safe_url(next_page):
+                return redirect(next_page)
+            return redirect(url_for('dashboard.index'))
         else:
-            flash("Ошибка авторизации. Проверьте email и пароль.", "danger")
+            flash("Authorization error. Check your email and password.", "danger")
     return render_template('main/login.html', form=form)
 
 
@@ -27,7 +37,6 @@ def login():
 @login_required
 def user_create():
     form = UserCreateForm()
-
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         user = Users(
@@ -37,13 +46,10 @@ def user_create():
             createdAt=datetime.utcnow(),
             updatedAt=datetime.utcnow()
         )
-
         db.session.add(user)
         db.session.commit()
-
         flash('Пользователь успешно создан!', 'success')
         return redirect(url_for('dashboard.index'))
-
     return render_template('users/create.html', form=form)
 
 
@@ -51,4 +57,5 @@ def user_create():
 @login_required
 def logout():
     logout_user()
+    flash("You have been logged out.", "info")
     return redirect(url_for('users.login'))
