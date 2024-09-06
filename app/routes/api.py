585aa0590b7899b3bf91d2dfa0c5b494a6c4523a
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-09-07 00:43:50 +0400

    update api test

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,15 +1,26 @@
-from flask import Blueprint, request, render_template, redirect, url_for, flash, session, g
-from flask_login import login_user, login_required, current_user, logout_user
-import sqlalchemy as sa
-from ..extensions import db, bcrypt
-from ..forms.users import UserCreateForm, LoginForm
-from ..models.users import Users
-from datetime import datetime
-from urllib.parse import urlparse, urljoin
+import os
+import json
+from flask import Blueprint, request, jsonify
 
 api = Blueprint('api', __name__)
 
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-@api.route('/api/test', methods=['GET', 'POST'])
-def login():
-    return {'test': 'test'}
+@api.route('/api/test', methods=['POST'])
+def save_data():
+    if request.method == 'POST':
+        data = request.get_json()
+
+        if not data:
+            return jsonify({'error': 'No data provided'})
+
+
+        file_path = os.path.join(UPLOAD_FOLDER, 'data.json')
+
+
+        with open(file_path, 'a') as f:
+            f.write(json.dumps(data) + '\n')
+
+        return jsonify({'message': 'Data saved successfully', 'file_path': file_path})
+
+    return jsonify({'error': 'Invalid request method'})
