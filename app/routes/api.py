47c03208f5b83cfc5dd2612fd7100c09c603425c
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-09-07 08:32:03 +0400

    test

--- app/routes/api.py
+++ app/routes/api.py
@@ -6,21 +6,25 @@ api = Blueprint('api', __name__)
 
 UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-@api.route('/api/test', methods=['POST', 'GET'])
+
+@api.route('/api/test', methods=['POST', 'GET', 'PUT'])
 def save_data():
     if request.method == 'POST':
+        # Получаем данные в формате JSON
         data = request.get_json()
 
+        # Проверяем, что данные переданы
         if not data:
-            return jsonify({'error': 'No data provided'})
+            return jsonify({'error': 'No data provided'}), 400
 
-
+        # Указываем путь к файлу, куда будем сохранять данные
         file_path = os.path.join(UPLOAD_FOLDER, 'data.json')
 
-
+        # Сохраняем данные в файл
         with open(file_path, 'a') as f:
             f.write(json.dumps(data) + '\n')
 
-        return jsonify({'message': 'Data saved successfully', 'file_path': file_path})
+        # Возвращаем успешный ответ
+        return jsonify({'message': 'Data saved successfully', 'file_path': file_path}), 200
 
-    return jsonify({'error': 'Invalid request method'})
+    return jsonify({'error': 'Invalid request method'}), 405
