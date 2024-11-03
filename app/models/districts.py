4de7ec8a026523c1e491a1fff627f13c59710313
Giorgi Chighladze <80564127+chighladze@users.noreply.github.com>
2024-11-03 22:27:17 +0400

    fix

--- app/models/districts.py
+++ app/models/districts.py
@@ -8,7 +8,7 @@ class District(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
-    region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), default=1, nullable=False)
+    region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
